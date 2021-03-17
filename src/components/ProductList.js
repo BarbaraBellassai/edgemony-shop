8ce05b6d2b5d0be952faaed6945ec441ac51c403
Barbara Bellassai <b.bellassai88@gmail.com>
2021-03-17 23:29:18 +0100

    Categories filter updated

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,13 +2,45 @@ import { useState } from "react";
 import { PropTypes } from "prop-types";
 import Product from "./Product";
 import Search from "./Search";
+import {useLocation, useHistory} from "react-router-dom";
 
 import "./ProductList.css";
 import CategoriesFilter from "./CategoriesFilter";
 
 function ProductList({ products, categories }) {
-  const [searchTerm, setSearchTerm] = useState();
-  const [selectedCategories, setSelectedCategories] = useState([]);
+  // const [searchTerm, setSearchTerm] = useState();
+  // const [selectedCategories, setSelectedCategories] = useState([]);
+
+  const location = useLocation();
+  const history = useHistory();
+
+  const searchParams = new URLSearchParams(location.search);
+
+  const searchTerm = searchParams.get("q") || "";
+
+  function updateSearchTerm(term) {
+    if (term) {
+      searchParams.set("q", term);
+    } else {
+      searchParams.delete("q");
+    }
+    history.push({search: "?" + searchParams.toString()});
+  }
+
+  const selectedCategoriesParam = searchParams.get("categories");
+  const selectedCategories = selectedCategoriesParam
+  ? selectedCategoriesParam.split(",")
+  : [];
+
+  function updateCategories(categories) {
+    const selectedParam = categories.join(",");
+    if (categories.length === 0){
+      searchParams.delete("categories");
+    }else{
+      searchParams.set("categories", selectedParam);
+    }
+    history.push({search: "?" + searchParams.toString()});
+  }
 
   const termRegexp = new RegExp(searchTerm, "i");
   const filteredProducts = products.filter(
@@ -21,11 +53,11 @@ function ProductList({ products, categories }) {
   return (
     <div className="ProductList">
       <div className="ProductList__filters">
-        <Search onSearch={setSearchTerm} />
+        <Search term={searchTerm} onSearch={updateSearchTerm} />
         <CategoriesFilter
           categories={categories}
           selectedCategories={selectedCategories}
-          onSelectCategory={setSelectedCategories}
+          onSelectCategory={updateCategories}
         />
       </div>
       <div className="ProductList__products">
